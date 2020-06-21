afe25e7c4a9519d7a221c4fabb27a3b9db1ed085
yusshu <yushuverduras1309@gmail.com>
2020-06-21 16:01:38 -0500

    Add simple test

---
+++ core/src/test/java/me/yushust/inject/test/InstanceBindingTest.java
@@ -0,0 +1,40 @@
+package me.yushust.inject.test;
+
+import me.yushust.inject.Inject;
+import me.yushust.inject.Injector;
+import me.yushust.inject.InjectorFactory;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class InstanceBindingTest {
+
+    @Test
+    public void testInstanceBinding() {
+
+        Foo foo = new Foo();
+
+        Injector injector = InjectorFactory.create(binder -> {
+            binder.bind(Foo.class).toInstance(foo);
+        });
+
+        injector.injectMembers(foo);
+        assertNotNull(foo.bar);
+        assertEquals(foo, foo.bar.foo);
+
+    }
+
+    public static class Foo {
+
+        @Inject private Bar bar;
+
+    }
+
+    public static class Bar {
+
+        @Inject private Foo foo;
+
+    }
+
+}
