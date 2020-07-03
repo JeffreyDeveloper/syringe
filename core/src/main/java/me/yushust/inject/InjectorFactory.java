ffd942e9099344f63b805894be0963d14a460ec7
yusshu <yushuverduras1309@gmail.com>
2020-07-03 11:55:41 -0500

    Added documentation for InjectorFactory and Provider

--- core/src/main/java/me/yushust/inject/InjectorFactory.java
+++ core/src/main/java/me/yushust/inject/InjectorFactory.java
@@ -10,24 +10,71 @@ import me.yushust.inject.resolve.resolver.*;
 
 import java.util.Arrays;
 
+/**
+ * A factory to create injectors in a very simple way
+ * <p>
+ *     Module myModule = new MyModule();
+ *     Injector injector = InjectorFactory.create(myModule);
+ * </p>
+ * You can also use more options using {@link InjectorOptions}
+ * <p>
+ *
+ *     Module myModule = new MyModule();
+ *     Injector injector = InjectorFactory.create(
+ *          InjectorOptions.builder()
+ *              .optionalInjectionChecker(new MyOptionalInjectionChecker())
+ *              .build(),
+ *          myModule
+ *     );
+ *
+ * </p>
+ * @see Injector
+ */
 public final class InjectorFactory {
 
     private InjectorFactory() {
         throw new UnsupportedOperationException("This class couldn't be instantiated!");
     }
 
+    /**
+     * Create an {@link Injector} with the given Modules.
+     * Equivalent to {@link InjectorFactory#create(InjectorOptions, Module...)}
+     * using default {@link InjectorOptions}
+     * @param modules Injector configuration modules
+     * @return The injector
+     */
     public static Injector create(Module... modules) {
         return create(InjectorOptions.builder().build(), modules);
     }
 
+    /**
+     * Create an {@link Injector} with the given Modules and given Injector Options
+     * Equivalent to {@link InjectorFactory#create(InjectorOptions, Iterable)}
+     * @param options Injector options
+     * @param modules Injector configuration modules
+     * @return The injector
+     */
     public static Injector create(InjectorOptions options, Module... modules) {
         return create(options, Arrays.asList(modules));
     }
 
+    /**
+     * Create an {@link Injector} with the given Modules.
+     * Equivalent to {@link InjectorFactory#create(InjectorOptions, Iterable)}
+     * using default {@link InjectorOptions}
+     * @param modules Injector configuration modules
+     * @return The injector
+     */
     public static Injector create(Iterable<Module> modules) {
         return create(InjectorOptions.builder().build(), modules);
     }
 
+    /**
+     * Create an {@link Injector} with the given Modules and the given options.
+     * @param options Injector options
+     * @param modules Injector configuration modules
+     * @return The injector
+     */
     public static Injector create(InjectorOptions options, Iterable<Module> modules) {
 
         InternalBinder binder = new SimpleBinder();
--- core/src/main/java/me/yushust/inject/Provider.java
+++ core/src/main/java/me/yushust/inject/Provider.java
@@ -1,7 +1,22 @@
 package me.yushust.inject;
 
+import me.yushust.inject.bind.Binding;
+import me.yushust.inject.scope.Scope;
+
+/**
+ * A functional interface that provides instances can be wrapped
+ * with a {@link Scope} (like {@code Scopes.SINGLETON})
+ * @param <T> The type of instance that will be provided
+ * @see Binding
+ */
 public interface Provider<T> {
 
+    /**
+     * Get the instance that this Provider gives you. It can always
+     * be a new instance or always the same instance, depending on
+     * the Scope and as defined by the user
+     * @return An object
+     */
     T get();
 
 }
